36eecb426dc08897ee225f7ba026a29dcbe52cc6
Kebigon <git@kebigon.xyz>
2021-02-07 17:40:50 +0900

    Ignore sentence cards with tags TooLong or TooShort

--- japwords/src/main/java/xyz/kebigon/vra/japwords/anki/AnkiDatabase.java
+++ japwords/src/main/java/xyz/kebigon/vra/japwords/anki/AnkiDatabase.java
@@ -21,7 +21,8 @@ public class AnkiDatabase implements AutoCloseable
 
 	public boolean hasIPlusOneSentence(WordDefinition word)
 	{
-		try (PreparedStatement ps = connection.prepareStatement("SELECT COUNT(*) FROM notes WHERE tags LIKE '%i+1%' AND flds LIKE ?"))
+		try (PreparedStatement ps = connection.prepareStatement(
+				"SELECT COUNT(*) FROM notes WHERE tags LIKE '%i+1%' AND tags NOT LIKE '%TooLong%' AND tags NOT LIKE '%TooShort%' AND flds LIKE ?"))
 		{
 			ps.setString(1, "%" + word.getWord() + "%");
 			final ResultSet rs = ps.executeQuery();
